3ea8d649cae46868a8e48b2da18bd7367e4a3922
stkevintan <stkevintan@zju.edu.cn>
2025-12-30 12:31:35 +0800

    send real song count when getting scanStatus

--- server/subsonic/scan.go
+++ server/subsonic/scan.go
@@ -7,14 +7,20 @@ import (
 	"github.com/stkevintan/miko/models"
 	"github.com/stkevintan/miko/pkg/di"
 	"github.com/stkevintan/miko/pkg/scanner"
+	"gorm.io/gorm"
 )
 
 func (s *Subsonic) handleGetScanStatus(w http.ResponseWriter, r *http.Request) {
 	sc := di.MustInvoke[*scanner.Scanner](r.Context())
+	db := di.MustInvoke[*gorm.DB](r.Context())
+
+	var count int64
+	db.Model(&models.Child{}).Where("is_dir = ?", false).Count(&count)
+
 	resp := models.NewResponse(models.ResponseStatusOK)
 	resp.ScanStatus = &models.ScanStatus{
 		Scanning: sc.IsScanning(),
-		Count:    sc.ScanCount(),
+		Count:    count,
 	}
 	s.sendResponse(w, r, resp)
 }
@@ -22,12 +28,18 @@ func (s *Subsonic) handleGetScanStatus(w http.ResponseWriter, r *http.Request) {
 func (s *Subsonic) handleStartScan(w http.ResponseWriter, r *http.Request) {
 	incremental := isPositive(r.URL.Query().Get("inc"))
 	sc := di.MustInvoke[*scanner.Scanner](r.Context())
+	db := di.MustInvoke[*gorm.DB](r.Context())
+
+	var count int64
+	db.Model(&models.Child{}).Where("is_dir = ?", false).Count(&count)
+
 	// r.Context() may destroy on client disconnect, so create a new background context
 	ctx := di.Inherit(context.Background(), r.Context())
 	go sc.Scan(ctx, incremental)
 	resp := models.NewResponse(models.ResponseStatusOK)
 	resp.ScanStatus = &models.ScanStatus{
 		Scanning: true,
+		Count:    count,
 	}
 	s.sendResponse(w, r, resp)
 }
